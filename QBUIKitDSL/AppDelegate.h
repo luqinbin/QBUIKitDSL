a03cf2388a8d0d064f0f66892c118bd30c8b16c7
覃斌 卢 <luqinbin@pwrd.com>
2020-01-09 13:47:21 +0800

    Initial Commit

---
+++ QBUIKitDSL/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QBUIKitDSL
+//
+//  Created by 覃斌 卢    on 2020/1/9.
+//  Copyright © 2020 覃斌 卢   . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ QBUIKitDSL/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  QBUIKitDSL
+//
+//  Created by 覃斌 卢    on 2020/1/9.
+//  Copyright © 2020 覃斌 卢   . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
